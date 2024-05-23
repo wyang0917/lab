71cbb81cefd6a64b39b0c5b96760abc4e40b0b66
wyang0917 <yangwang0917@gmail.com>
2024-05-23 21:21:24 +1000

    create pagination practice

---
+++ pagination/src/components/Pagination.jsx
@@ -0,0 +1,21 @@
+const Pagination =({paginate,postsPerPage,currentPage,totalPosts})=>{
+  const pageNumbers=[]
+  for(let i =1;i<=Math.ceil(totalPosts/postsPerPage);i++){
+    pageNumbers.push(i)
+  }
+  return( <nav>
+    <ul className="pagination">
+      {
+        pageNumbers.map((number)=>(
+          <li key={number} className="page-item">
+          <a
+          onClick={()=>paginate(number)} 
+          href="#" className={`page-link ${currentPage===number ? "active-page" : ""}`}>{number}</a>
+        </li>
+        ))
+      }
+    </ul>
+  </nav>)
+}
+
+export default Pagination
